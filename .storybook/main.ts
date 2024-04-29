efa8a95277f00a58eecc4ad06c9ab447ede018b4
nzkks <kkshanthosh@gmail.com>
2024-04-30 11:51:55 +1200

    main.ts file in .storybook folder: accepts typescript files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/components/**/*.stories.@(js|jsx)'],
+  stories: ['../src/components/**/*.stories.@(ts|tsx)'],
   staticDirs: ['../public'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
