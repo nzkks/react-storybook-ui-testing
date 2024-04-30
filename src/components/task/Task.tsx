39b6c49981bf00b3fc36518af54e3bfc08acc050
nzkks <kkshanthosh@gmail.com>
2024-04-30 12:34:22 +1200

    Task component: Add a checkbox and pin button

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -8,10 +8,33 @@ type TaskProps = {
 
 export const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }: TaskProps) => {
   return (
-    <div className="list-item">
-      <label htmlFor="title" aria-label={title}>
-        <input type="text" value={title} readOnly={true} name="title" />
+    <div className={`list-item ${state}`}>
+      <label htmlFor="checked" aria-label={`archiveTask-${id}`} className="checkbox">
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === 'TASK_ARCHIVED'}
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
+
+      <label htmlFor="title" aria-label={title} className="title">
+        <input type="text" value={title} readOnly={true} name="title" placeholder="Input title" />
+      </label>
+
+      {state !== 'TASK_ARCHIVED' && (
+        <button
+          key={`pinTask-${id}`}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   );
 };
